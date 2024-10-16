f25d3de9987c585a1a27aa1207be5c9411d7fa7d
leeahl <annaleehalim@gmail.com>
2024-10-16 11:56:22 +0900

    백준 1333

--- src/부재중전화.java
+++ src/부재중전화.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class 부재중전화 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+        int D = sc.nextInt();
+        int result = L;
+        int d = D;
+        int i =0;
+        while (i < N) {
+            if (result<=d && d<result+5)break;
+            if (result > d) {
+                while (d < result) {
+                    d += D;
+                }
+            }else {
+                i++;
+                result += L + 5;
+            }
+        }
+
+        System.out.println(d);
+    }
 }
