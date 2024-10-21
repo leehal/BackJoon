cfec67537a65fd9cf39e80dccb4eadbb957c1bc0
leeahl <annaleehalim@gmail.com>
2024-10-21 14:07:54 +0900

    백준 1546

--- src/수열의변화.java
+++ src/수열의변화.java
@@ -1,5 +1,13 @@
-public class 수열의변화 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class 수열의변화 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] num = bf.readLine().split(" ");
+        bf.readLine();
+        String[] arr = bf.readLine().split(",");
+        System.out.println(Integer.parseInt(num[0])+Integer.parseInt(arr[0]));
     }
 }
