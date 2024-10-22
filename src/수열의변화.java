a8cc9473e779ea9695d6eaacf5c069224b593246
leeahl <annaleehalim@gmail.com>
2024-10-22 10:40:51 +0900

    백준 1551

--- src/수열의변화.java
+++ src/수열의변화.java
@@ -6,8 +6,21 @@ public class 수열의변화 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] num = bf.readLine().split(" ");
-        bf.readLine();
         String[] arr = bf.readLine().split(",");
-        System.out.println(Integer.parseInt(num[0])+Integer.parseInt(arr[0]));
+        int[] result = new int[Integer.parseInt(num[0])];
+        for (int i = 0; i < Integer.parseInt(num[1]); i++) {
+            for (int j = 1; j < Integer.parseInt(num[0])-i; j++) {
+                result[j - 1] = Integer.parseInt(arr[j]) - Integer.parseInt(arr[j-1]);
+                arr[j-1]= String.valueOf(result[j-1]);
+            }
+            
+        }
+        for (int i = 0; i < Integer.parseInt(num[0])-Integer.parseInt(num[1]); i++) {
+            if (i == Integer.parseInt(num[0]) - Integer.parseInt(num[1]) - 1) {
+                System.out.print(arr[i]);
+            }else {
+                System.out.print(arr[i]+",");
+            }
+        }
     }
 }
